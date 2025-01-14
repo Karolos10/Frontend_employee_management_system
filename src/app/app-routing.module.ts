d49ac3df0f92233fa0097ac8277a4b8da36de8e1
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-14 17:29:05 -0500

    Implementation of a service to view employee details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'update-employee/:id',
     component: UpdateEmployeeComponent
+  },
+  {
+    path: 'employee-details/:id',
+    component: EmployeeDetailsComponent
   },
   {
     path: '',
