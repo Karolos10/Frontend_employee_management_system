a71c86616c94831ff8d4b004825eb11099230747
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-14 15:23:32 -0500

    service creation, http module injection and user view formating

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
 
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  }
 }
