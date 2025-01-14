06cb4eaa12d71ea5d602c837f58d49d6c1e2357e
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-14 16:06:31 -0500

    creation of a form to enter a new employee

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-create-employee',
+  standalone: false,
+
+  templateUrl: './create-employee.component.html',
+  styleUrl: './create-employee.component.css'
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.employee);
+  };
+
+}
